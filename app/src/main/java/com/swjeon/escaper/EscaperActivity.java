3e1aedf6d574f7f15dac01ae89fc2eb096b97c89
jsw5514 <jsw55514@naver.com>
2025-05-10 00:24:20 +0900

    게임 액티비티를 상속하도록 변경, 빈 신을 하나 푸시하도록 설정

--- app/src/main/java/com/swjeon/escaper/EscaperActivity.java
+++ app/src/main/java/com/swjeon/escaper/EscaperActivity.java
@@ -2,12 +2,14 @@ package com.swjeon.escaper;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 
-public class EscaperActivity extends AppCompatActivity {
+public class EscaperActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new Scene().push();
     }
 }
