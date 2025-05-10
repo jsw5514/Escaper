22d56f38e2ee94b25211d25a681b228101af684b
jsw5514 <jsw5524@gmail.com>
2025-05-10 15:46:07 +0900

    게임 좌표계를 1000*1000으로 설정

--- app/src/main/java/com/swjeon/escaper/EscaperActivity.java
+++ app/src/main/java/com/swjeon/escaper/EscaperActivity.java
@@ -5,13 +5,14 @@ import android.os.Bundle;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.Metrics;
 
 public class EscaperActivity extends GameActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GameView.drawsDebugStuffs = BuildConfig.DEBUG;
+        Metrics.setGameSize(1000,1000);
         new Scene().push();
     }
 }
