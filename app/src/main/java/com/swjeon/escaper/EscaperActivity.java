e8b4a29271f79a675ae562bb0980273682ff7489
jsw5514 <jsw55514@naver.com>
2025-05-10 13:01:10 +0900

    디버그 정보 출력

--- app/src/main/java/com/swjeon/escaper/EscaperActivity.java
+++ app/src/main/java/com/swjeon/escaper/EscaperActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.activity.GameActivity;
 import kr.ac.tukorea.ge.spgp2025.a2dg.framework.scene.Scene;
+import kr.ac.tukorea.ge.spgp2025.a2dg.framework.view.GameView;
 
 public class EscaperActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        GameView.drawsDebugStuffs = BuildConfig.DEBUG;
         new Scene().push();
     }
 }
