0061895105dfe37112bf3b47250ac0efc26c7acf
jsw5514 <jsw55514@naver.com>
2025-05-17 19:08:54 +0900

    뒤집힌 타일 정상출력하도록 수정+Rect 객체 반복 생성 방지 최적화

--- README.md
+++ README.md
@@ -169,10 +169,6 @@ MainScene에 등장하는 game object에 대하여<br>
 <br>
 
 #### Todo List ####
-TildeMap.java<br>
-타일맵에서 뒤집힌 타일을 정상적으로 로딩하도록 수정(현재는 플래그만 제거하여 뒤집힌 타일을 뒤집히지 않은 상태로 출력)<br>
-drawTileAt에서 Rect 객체를 반복생성하지 않도록 최적화 필요<br>
-<br>
 TiledMapManager.java<br>
 생성자에서 넘겨받는 tilesetResId를 이용해 TileSet 객체를 생성 및 초기화(필요시 타일셋 파일명도 생성자 인자로 추가)<br>
 
--- app/src/main/java/com/swjeon/escaper/game/TiledMap.java
+++ app/src/main/java/com/swjeon/escaper/game/TiledMap.java
@@ -25,6 +25,10 @@ public class TiledMap implements IGameObject {
     private static final int FLIPPED_H = 0x80000000;
     private static final int FLIPPED_V = 0x40000000;
     private static final int FLIPPED_D = 0x20000000;
+
+    //for draw
+    Rect srcRect = new Rect();
+    RectF dstRect = new RectF();
     public TiledMap(int[] tileDatas, TileSet tileSet){
         this(tileDatas, tileSet, 1); //명시적인 지정이 없으면 gid 시작을 기본값인 1로 사용
     }
@@ -51,6 +55,12 @@ public class TiledMap implements IGameObject {
     private void drawTileAt(Canvas canvas, int rawGid, float x, float y) {
         //gid 내 플래그 제거
         int gid = rawGid & ~(FLIPPED_H | FLIPPED_V | FLIPPED_D);
+
+        //플래그 확인
+        boolean isFlippedH = (rawGid & FLIPPED_H) != 0;
+        boolean isFlippedV = (rawGid & FLIPPED_V) != 0;
+        boolean isFlippedD = (rawGid & FLIPPED_D) != 0;
+
         //0-based 인덱스로 변경
         int tileIndex = gid - firstGid;
         if (tileIndex < 0) return;  //빈 공간일 때는 아무것도 그리지 않음
@@ -58,12 +68,21 @@ public class TiledMap implements IGameObject {
         //src Rect 계산
         int srcX = (tileIndex % (int)tileSet.getColumns()) * (int)tileSet.getTilewidth();
         int srcY = (tileIndex / (int)tileSet.getColumns()) * (int)tileSet.getTileheight();
-        Rect src = new Rect(srcX, srcY, srcX + (int)tileSet.getTilewidth(), srcY + (int)tileSet.getTileheight());
+        srcRect.set(srcX, srcY, srcX + (int)tileSet.getTilewidth(), srcY + (int)tileSet.getTileheight());
 
-        //dst Rect 계산
-        RectF dst = new RectF(x, y, x + MAP_TILE_WIDTH, y + MAP_TILE_WIDTH);
 
-        canvas.drawBitmap(tileSetImg, src, dst, null);
+        //flip 플래그 적용하여 dst Rect 계산
+        canvas.save();
+        canvas.translate(x + MAP_TILE_WIDTH / 2f, y + MAP_TILE_WIDTH / 2f); //그려야 하는 타일의 중심으로 원점 이동
+        if(isFlippedD){ //대각선 뒤집기 처리
+            canvas.rotate(90);
+        }
+        float sx = isFlippedH ? -1f : 1f; //세로 뒤집기 처리
+        float sy = isFlippedV ? -1f : 1f; //가로 뒤집기 처리
+        canvas.scale(sx, sy);
+        dstRect.set(-MAP_TILE_WIDTH/2f, -MAP_TILE_WIDTH/2f, MAP_TILE_WIDTH/2f, MAP_TILE_WIDTH/2f); //현재 타일의 정중앙에 있으므로 왼쪽 위부터 그려야한다.
+        canvas.drawBitmap(tileSetImg, srcRect, dstRect, null);
+        canvas.restore();
     }
 
 }
