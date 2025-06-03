7c3768ed4833edf25dcf7c74a27efe5bfda380d0
jsw5514 <jsw5524@gmail.com>
2025-06-03 22:19:07 +0900

    맵 데이터에서 오브젝트 레이어 로딩하도록 추가 완료

---
+++ app/src/main/java/com/swjeon/escaper/game/map/EnemySpawnInfo.java
@@ -0,0 +1,12 @@
+package com.swjeon.escaper.game.map;
+
+import android.graphics.Path;
+import android.graphics.Point;
+
+public class EnemySpawnInfo {
+    public Point startPosition;
+    public Path patrolPath;
+
+    public EnemySpawnInfo() {
+    }
+}
--- app/src/main/java/com/swjeon/escaper/game/map/TiledMapManager.java
+++ app/src/main/java/com/swjeon/escaper/game/map/TiledMapManager.java
@@ -1,6 +1,8 @@
 package com.swjeon.escaper.game.map;
 
 import android.content.Context;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.util.Log;
 
 import com.swjeon.escaper.R;
@@ -21,9 +23,14 @@ public class TiledMapManager {
     private final String TAG = this.getClass().getSimpleName();
     private final Context context;
     private final static int[] MAP_IDS = new int[]{R.raw.maze_1,R.raw.maze_2,R.raw.maze_3}; //맵 json 파일 리소스 번호
-    private ArrayList<TiledMap> maps;
+
+    //map datas
+    private ArrayList<TiledMap> maps = new ArrayList<>();
+    private ArrayList<Point> playerStarts = new ArrayList<>();
+    private ArrayList<ArrayList<EnemySpawnInfo>> enemyOffsets = new ArrayList<>();
     private final float MAP_TILE_WIDTH; //화면에서 타일 하나의 가로, 세로 길이
     private TileSet tileSet;
+
     public TiledMapManager(Context appContext, int tileSetImgId, int tileSetJsonId, float mapTileWidth){
         this.context = appContext;
         this.MAP_TILE_WIDTH = mapTileWidth;
@@ -96,7 +103,6 @@ public class TiledMapManager {
         }
 
         //가져온 json 객체에서 맵 데이터 추출(단일 레이어, 단일 타일셋 맵을 가정)
-        maps = new ArrayList<>();
         for(JSONObject jsonMapData :jsonMapDatas){
             try {
                 //load layers
@@ -114,8 +120,53 @@ public class TiledMapManager {
                 }
                 maps.add(new TiledMap(mapData, tileSet, mapSize, MAP_TILE_WIDTH, firstGid));
 
-                //load objects
+                //load object layer
                 JSONArray jsonGObjects = layers.getJSONObject(1).getJSONArray("objects");
+                Point playerStart = new Point();
+                ArrayList<EnemySpawnInfo> enemySpawnInfos = new ArrayList<>();
+                for(int i = 0; i<jsonGObjects.length(); i++){
+                    JSONObject item = jsonGObjects.getJSONObject(i);
+                    int enemyIndex;
+                    switch (item.getString("type")){
+                        case "player":
+                            int x = item.getInt("x");
+                            int y = item.getInt("y");
+                            playerStart.set(x,y);
+                            break;
+                        case "enemy":
+                            //ensure size of list
+                            enemyIndex = Integer.valueOf(item.getString("name"));
+                            while(enemySpawnInfos.size() < enemyIndex){
+                                enemySpawnInfos.add(new EnemySpawnInfo());
+                            }
+                            //parse enemy pos data
+                            int enemyX = item.getInt("x");
+                            int enemyY = item.getInt("y");
+                            enemySpawnInfos.get(enemyIndex).startPosition = new Point(enemyX,enemyY);
+                        case "path":
+                            //ensure size of list
+                            enemyIndex = Integer.valueOf(item.getString("name"));
+                            while(enemySpawnInfos.size() < enemyIndex){
+                                enemySpawnInfos.add(new EnemySpawnInfo());
+                            }
+                            //parse enemy patrol path data
+                            Path patrolPath = new Path();
+                            int pathOffsetX = item.getInt("x");
+                            int pathOffsetY = item.getInt("y");
+                            JSONArray pathPoints = item.getJSONArray("polyline");
+                            for(int j=0; j<pathPoints.length(); j++){
+                                JSONObject pathPoint = pathPoints.getJSONObject(j);
+                                int pathPointX = pathPoint.getInt("x") + pathOffsetX;
+                                int pathPointY = pathPoint.getInt("y") + pathOffsetY;
+                                if(j==0) patrolPath.moveTo(pathPointX,pathPointY);
+                                else patrolPath.lineTo(pathPointX,pathPointY);
+                            }
+                            enemySpawnInfos.get(enemyIndex).patrolPath = patrolPath;
+                    }
+                }
+                playerStarts.add(playerStart);
+                enemyOffsets.add(enemySpawnInfos);
+
                 //TODO create map data bundle
             } catch (JSONException e) {
                 Log.e(TAG,"error while converting json to map bundle");
